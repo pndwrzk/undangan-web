05b3d88a90f678ca5fd774fe9816bd5d78b815b2
Rizki pandiwa <pandiwarizki.rp@gmail.com>
2026-03-27 10:53:31 +0700

    chore: Add `10.147.252.254` to allowed development origins.

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   allowedDevOrigins: [
-    "http://103.171.85.219:3000",
     "http://localhost:3000",
+    "http://10.147.252.254:3000",
+    "http://103.171.85.219:3000",
   ],
 };
 
