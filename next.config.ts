7012dd2b78c3aff9be21cbfd0da9280a0b251497
Rizki pandiwa <pandiwarizki.rp@gmail.com>
2026-04-12 09:56:16 +0700

    chore(config): add ngrok domain to allowed origins

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,7 @@ const nextConfig: NextConfig = {
     "http://localhost:3000",
     "http://10.147.252.254:3000",
     "http://103.171.85.219:3000",
+    "footer-fifth-napkin.ngrok-free.dev"
   ],
 };
 
