9d20b7744327746e744df66726fddacdee8e2326
Rizki pandiwa <pandiwarizki.rp@gmail.com>
2026-04-24 14:10:11 +0700

    add allow

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   allowedDevOrigins: [
     "http://localhost:3000",
-     "http://103.193.179.119",
+     "http://103.193.179.119:3004",
     "http://10.147.252.254:3000",
     "http://103.171.85.219:3000",
     "footer-fifth-napkin.ngrok-free.dev",
